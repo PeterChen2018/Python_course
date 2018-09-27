fb4bdb337ac12450fe160830ede27e66972cf90f
Peter Chen <twstock99@gmail.com>
2018-09-27 11:54:37 +0800

    peter

---
+++ djangoenv/ch08p27/news/newsapp/admin.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from newsapp import models
+
+admin.site.register(models.NewsUnit)
---
+++ djangoenv/ch09p27/board/boardapp/admin.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from boardapp import models
+
+admin.site.register(models.BoardUnit)

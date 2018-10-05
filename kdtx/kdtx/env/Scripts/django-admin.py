542d55dd2a1c7695e619a892e0683c99785e29b4
Peter Chen <twstock99@gmail.com>
2018-10-05 13:18:57 +0800

    peter

--- kdtx/kdtx/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\peter\desktop\git\python_course\kdtx\kdtx\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

f449acab694a5124e379621c2231e3029c65baf7
Peter Chen <twstock99@gmail.com>
2018-10-05 14:08:36 +0800

    peter

--- kdtx/kdtx/kdtx/urls.py
+++ kdtx/kdtx/kdtx/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from kucun.views import all_goods
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', all_goods ),
 ]
--- kdtx/kdtx/kucun/views.py
+++ kdtx/kdtx/kucun/views.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from .models import Goods, Shop, GoodsShop, GoodsRecord, Backup, GoodsAddRecord, GoodsSellRecord, InboundChannel, \
+    Cart, Order, GoodsReturnRecord, OtherCost, TransferShop, TransferRecord, ChangeCountRecord, RefundRecord
 
-# Create your views here.
+
+def all_goods(request):
+    goods = Goods.objects.filter(is_delete=False).order_by('goods_name')
+    datas = []
+    amount = 0
+    for good in goods:
+        kadi = GoodsShop.objects.get(goods=good, shop__name='卡迪电子')
+        m = {'goods': good, 'kadi': kadi}
+        amount += kadi.remain
+        datas.append(m)
+    shang = len(goods) / 3
+    yu = len(goods) % 3
+    if yu != 0:
+        shang += 1
+
+    return render_to_response('all_goods.html',
+                              {'request': request, 'data1': datas[:shang], 'data2': datas[shang:shang * 2],
+                               'data3': datas[shang * 2:], 'title': '卡迪管理系统', 'header': '卡迪管理系统', 'amount': amount})
+

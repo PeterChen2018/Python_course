a7ee1fbbf3139d80a00576a4f2f6e122817b6358
Peter Chen <twstock99@gmail.com>
2018-10-05 11:52:38 +0800

    peter

---
+++ ypsi/ypsi/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\peter\desktop\git\python_course\ypsi\ypsi\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
